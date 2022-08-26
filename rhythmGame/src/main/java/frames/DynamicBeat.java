0e5a63361c817424c4c7b954a54477454ec27219
jingwook <qnfdkf153@gmail.com>
2022-08-26 16:32:20 +0900

    2강
    배경사진 넣기, 더블 버퍼링 : :사진을 화면에 그려주는 메서드

--- rhythmGame/src/main/java/frames/DynamicBeat.java
+++ rhythmGame/src/main/java/frames/DynamicBeat.java
@@ -2,9 +2,18 @@ package frames;
 
 import application.RhyThmGame;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.util.Objects;
 
 public class DynamicBeat extends JFrame {
+    private Image screenImage;
+    private Graphics screenGraphic;
+
+    private final Image introBackground;
+
     public DynamicBeat() {
         this.setTitle("Dynamic Beat");
         this.setSize(RhyThmGame.SCREENWIDTH, RhyThmGame.SCREENHEIGHT);
@@ -12,5 +21,20 @@ public class DynamicBeat extends JFrame {
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+
+        introBackground = new ImageIcon("./src/main/resources/introBackground.jpg")
+                .getImage();
+    }
+
+    public void paint(Graphics graphics) {
+        screenImage = createImage(RhyThmGame.SCREENWIDTH, RhyThmGame.SCREENHEIGHT);
+        screenGraphic = screenImage.getGraphics();
+        screenDraw(screenGraphic);
+        graphics.drawImage(screenImage, 0, 0, null);
+    }
+
+    private void screenDraw(Graphics screenGraphic) {
+        screenGraphic.drawImage(introBackground, 0, 0, null);
+        this.repaint();
     }
 }
